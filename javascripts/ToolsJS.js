7be3f6573a174e21609c9765372b1f195ea39ae4
leva5277 <srh6955133@live.cn>
2016-07-31 04:23:50 +0800

    Checkin
    Check in new Tool for unit exchange.

--- javascripts/ToolsJS.js
+++ javascripts/ToolsJS.js
@@ -37,3 +37,22 @@ function CalculateVolume() {
     }
     document.getElementById('vol').value = totalVol;
 }
+
+function ExhcnageUnit(){
+    var ori = parseFloat(document.getElementById('ori').value);
+    var result;
+    var condition = document.getElementById('unitExchangeType').value;
+    switch(condition){
+        case 'F2C': result = (ori-32)*5/9; 
+        break;
+        case 'C2F': result = ori*9/5+32;;
+        break;
+        case 'E2S': result = ori*2.54;
+        break;
+        case 'S2E': result = ori/2.54;
+        break;
+        default: break;
+    }
+    document.getElementById('res').value = result;
+
+}
