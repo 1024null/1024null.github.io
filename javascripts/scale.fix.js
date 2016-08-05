6384b177a26ea3a79d2893446396064b127ae4c5
leva5277 <srh6955133@live.cn>
2016-08-06 04:34:29 +0800

    Update Mobile Platform Layout
    Update Mobile Platform Layout:
    For iPhone and Android devices only.

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -7,12 +7,6 @@ if (navigator.userAgent.toLowerCase().match(/iPhone/i) || navigator.userAgent.to
     }
   }
 
-  /*document.getElementById('unitExchange').style.width = document.getElementById('header').style.width;*/
-  document.getElementById('unitExchange').style.width = '375px';
-  document.getElementById('unitExchange').removeAttribute('top');
-  document.getElementById('unitExchange').removeAttribute('left'); 
-  document.getElementById('unitExchange').removeAttribute('margin-top');
-  document.getElementById('unitExchange').removeAttribute('margin-left');
   document.addEventListener("gesturestart", gestureStart, false);
 }
 function gestureStart() {
@@ -21,4 +15,31 @@ function gestureStart() {
       metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
     }
   }
+}
+
+function toolMobileMode(){
+  if (navigator.userAgent.toLowerCase().match(/iPhone/i) || navigator.userAgent.toLowerCase().match(/android/i)) {
+
+    toolDivMoibleMode('unitExchange');
+    toolDivMoibleMode('coverShowCalculateScaleVolume');
+    toolDivMoibleMode('underConstruction');
+
+  }
+
+}
+
+function toolDivMoibleMode(divID){
+    /*document.getElementById('unitExchange').style.width = document.getElementById('header').style.width;*/
+  document.getElementById(divID).style.width = '100%';
+  document.getElementById(divID).style.height = window.screen.height;
+  document.getElementById(divID).removeAttribute('top');
+  document.getElementById(divID).removeAttribute('left'); 
+  document.getElementById(divID).removeAttribute('margin-top');
+  document.getElementById(divID).removeAttribute('margin-left');
+  document.getElementById(divID).style.top = 0;
+  document.getElementById(divID).style.left = 0;
+  document.getElementById(divID).style.marginLeft = '0px';
+  document.getElementById(divID).style.marginTop = '0px';
+
+
 }
