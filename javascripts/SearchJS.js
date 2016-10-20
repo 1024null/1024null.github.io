a160c8619a14bb33c1fe049be8557ef0916770f1
leva5277 <srh6955133@live.cn>
2016-10-21 02:04:41 +0800

    Fix
    Fix some codes;
    New designed page layout

--- javascripts/SearchJS.js
+++ javascripts/SearchJS.js
@@ -14,21 +14,3 @@ function Search(key_words) {
 
     window.open(redirection + key_words);
 }
-function ExhcnageUnit(){
-    var ori = parseFloat(document.getElementById('ori').value);
-    var result;
-    var condition = document.getElementById('unitExchangeType').value;
-    switch(condition){
-        case 'F2C': result = (ori-32)*5/9; 
-        break;
-        case 'C2F': result = ori*9/5+32;;
-        break;
-        case 'E2S': result = ori*2.54;
-        break;
-        case 'S2E': result = ori/2.54;
-        break;
-        default: break;
-    }
-    document.getElementById('res').value = result;
-
-}
